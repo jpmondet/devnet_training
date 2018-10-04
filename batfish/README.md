136243eced5980e8761416c93922a26577344a9d
jpmondet <jp@mondet.org>
2018-10-04 14:37:14 +0200

    Playing with batfish questions

---
+++ batfish/README.md
@@ -0,0 +1,40 @@
+# Testing Batfish with python Client
+
+## Batfish server
+
+docker run -p 9997:9997 -p 9996:9996 batfish/batfish
+
+## Snapshots
+
+Snapshots must be structured in a specific way.
+
+Example of structure :
+
+**Candidate directory**
+  - **configs subdirectory** (network devices)
+       - R1_candidate.cfg
+       - R2_candidate.cfg
+       - ....
+  - **hosts subdirectory** (servers)
+       - host1.json 
+       - host2.json
+       - ....
+  - **iptables** (optional for servers)
+       - host1.iptables
+       - host2.iptables
+       - ....
+  - **topology subdirectory** (also optional on a Routed topology cause Batfish can deduce from the interfaces IPs)
+
+Hosts & topology files are in ``json`` format.
+
+Iptables files can be dumped with ``iptables-save`` command.
+
+## Batfish Client
+
+Via the client (here python script), we can check facts about the snapshoted topology by asking "Questions".
+
+List of questions already developed : https://pybatfish.readthedocs.io/en/latest/questions.html#module-pybatfish.question.bfq
+
+
+
+
--- batfish/try_batfish.py
+++ batfish/try_batfish.py
@@ -6,26 +6,42 @@
 Trying batfish
 """
 from pybatfish.client.commands import *
+from pybatfish.client.session import Session
 from pybatfish.question.question import load_questions, list_questions
 from pybatfish.question import bfq
 from pybatfish.datamodel.flow import HeaderConstraints as header
+import logging
+
+logger = logging.getLogger(__name__)
+
+#bf_session = Session(logger)
+#bf_session.coordinatorHost = "172.17.0.2"
+#bf_session.coordinatorPort = "8888"
+#bf_session.apiKey = "6887facfc39dd574a914327eb31fd04427f25f4cbea6fb97"
+load_questions()
+
+bf_init_snapshot("./candidate", name='candidate')
+bf_set_snapshot('candidate')
+
+# IP Interfaces
+ip_owners_ans = bfq.ipOwners().answer()
+print(ip_owners_ans.frame())
+
+# 1 specific interface
+iface_ans = bfq.interfaceProperties(nodes='as1border1', interfaces='GigabitEthernet0/0', properties='all-prefixes').answer()
+print(iface_ans.frame())
+
+# Bgp peer config -> On Docs but looks like it's not implemented
+#bgp_peer = bfq.bgpPeerConfiguration(nodes='as1border1')
+#print(bgp_peer.frame())
+
+# Bgp properties
+print((bfq.bgpProperties(nodes='as1border1').answer()).frame())
+
+# Bgp session
+print((bfq.bgpSessionStatus(nodes='as1border1').answer()).frame())
 
 
+# Traceroute !!
+print((bfq.traceroute(startLocation='host1', headers={"dstIps": "ofLocation(host2)"}).answer()).frame())
 
-
-bf_session.coordinatorHost = "172.17.0.2"
-bf_session.coordinatorPort = "8888"
-bf_session.apiKey = "061161f30fdd2d299733513fc17a8c2a6f1a4230f4332617"
-
-load_questions(from_server=False)
-
-#bf_init_snapshot(notebooks, name='candidate')
-#bf_set_snapshot('candidate')
-
-#ip_owners_ans = bfq.ipOwners().answer()
-
-#ip_owners_ans.frame()
-
-#iface_ans = bfq.interfaceProperties(nodes='as1border1', interfaces='GigabitEthernet0/0', properties='all-prefixes').answer()
-
-#iface_ans.frame()
