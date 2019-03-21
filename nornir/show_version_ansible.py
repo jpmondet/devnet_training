9ed21c8f4a6b006523db18e896ff859ec5e3def0
jpmondet <jp@mondet.org>
2019-03-21 21:47:23 +0100

    [nornir] Fix ansible inventory usage (and updated to nornir > 2)

--- nornir/show_version_ansible.py
+++ nornir/show_version_ansible.py
@@ -3,19 +3,31 @@
     Simply get the OS version from a device 
 """
 
-from nornir.core import InitNornir
-from nornir.plugins.tasks.networking import napalm_cli
+import ipdb
+from nornir import InitNornir
+from nornir.plugins.tasks import networking
 from nornir.plugins.functions.text import print_result
 
 def main():
-    nr = InitNornir(config_file="config_ansible.yaml")
-    print(nr.inventory.to_dict())
-    nx_host = nr.filter(platform='nx-os')
-    print(nx_host.to_dict())
-    version = nx_host.run(task=napalm_cli, commands=['sh version'])
-    
+    #nr = InitNornir(config_file="config.yaml")
+    nr = InitNornir(
+        core={"num_workers": 5},
+        inventory={
+            "plugin": "nornir.plugins.inventory.ansible.AnsibleInventory",
+            "options": {
+                "hostsfile": "hosts"
+            }
+        }
+    )
+
+    #print(nr.inventory.hosts)
+   
+    #facts = nr.run(task=networking.napalm_get, getters=["facts"])
+    #print_result(facts)
+
+    version = nr.run(task=networking.napalm_cli, commands=['sh version'])
+
     print_result(version)
-    
 
 
 if __name__ == '__main__':
