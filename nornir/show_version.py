b92cc97e0668e5889cb1fe4bbe81180b3a85fb80
jpmondet <jp@mondet.org>
2019-03-28 21:12:26 +0100

    Convert to nornir 2.x

--- nornir/show_version.py
+++ nornir/show_version.py
@@ -3,12 +3,14 @@
     Simply get the OS version from a device 
 """
 
-from nornir.core import InitNornir
+#from nornir.core import InitNornir
+from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
 
 def main():
-    nr = InitNornir(config_file="config.yaml")
+    config = "config.yaml"
+    nr = InitNornir(config_file=config)
     nx_host = nr.filter(site='earth', role='spine')
     version = nx_host.run(task=netmiko_send_command, command_string='sh version')
     
