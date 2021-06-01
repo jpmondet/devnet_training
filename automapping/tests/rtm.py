3015df1d50b9f613743090163bba8b2994f3936e
jpmondet <jp@mondet.org>
2021-06-01 09:53:33 +0200

    [autom] Add quick test file that reinitialize all db collections

---
+++ automapping/tests/rtm.py
@@ -0,0 +1,42 @@
+# /usr/bin/env python3
+
+
+from pymongo import MongoClient, ASCENDING
+
+# https://pymongo.readthedocs.io/en/stable/tutorial.html
+
+AUTOMAPPING_DB = "mongodb://localhost:27017/"
+
+client = MongoClient(AUTOMAPPING_DB)
+
+db = client.automapping
+
+#print("\n\n\n\INDEX")
+#print(list(db.index_seq.find()))
+print("\n\n\n\nNODES")
+print(list(db.nodes.find()))
+print("\n\n\n\nIFACES")
+print(list(db.ifaces.find()))
+print("\n\n\n\nLINKS")
+print(list(db.links.find()))
+print("\n\n\n\nSTATS")
+print(list(db.stats.find()))
+print("\n\n\n\UTILIZATION")
+print(list(db.utilization.find()))
+
+db.nodes.delete_many({})
+db.ifaces.delete_many({})
+db.links.delete_many({})
+db.stats.delete_many({})
+db.utilization.delete_many({})
+
+print("\n\n\n\nNODES")
+print(list(db.nodes.find()))
+print("\n\n\n\nIFACES")
+print(list(db.ifaces.find()))
+print("\n\n\n\nLINKS")
+print(list(db.links.find()))
+print("\n\n\n\nSTATS")
+print(list(db.stats.find()))
+print("\n\n\n\UTILIZATION")
+print(list(db.utilization.find()))
