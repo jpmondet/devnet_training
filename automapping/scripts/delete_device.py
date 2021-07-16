889c52d350bed7a40123277a86cfc771b792fa6d
jpmondet <jp@mondet.org>
2021-07-16 12:06:52 +0200

    [autom] Black'd delete_device script

--- automapping/scripts/delete_device.py
+++ automapping/scripts/delete_device.py
@@ -7,18 +7,19 @@ import re
 from random import randint
 from time import time
 from argparse import ArgumentParser
-from pymongo import MongoClient, ASCENDING, UpdateMany # type: ignore
-from pymongo.errors import DuplicateKeyError # type: ignore
+from pymongo import MongoClient, ASCENDING, UpdateMany  # type: ignore
+from pymongo.errors import DuplicateKeyError  # type: ignore
 
 # https://pymongo.readthedocs.io/en/stable/tutorial.html
 
 DB_STRING = getenv("DB_STRING")
 if not DB_STRING:
     DB_STRING = "mongodb://localhost:27017/"
-    #DB_STRING = "mongodb://mongodb:27017/"
+    # DB_STRING = "mongodb://mongodb:27017/"
 
 client = MongoClient(DB_STRING)
 db = client.automapping
+
 
 def delete_node_from_db(node_name: str) -> None:
 
@@ -28,9 +29,11 @@ def delete_node_from_db(node_name: str) -> None:
     db.stats.delete_many({"device_name": node_name})
     db.utilization.delete_many({"neighbor_name": node_name})
 
+
 def main():
     parser = ArgumentParser(
-        prog="db_filler", description="Fill db with fake datas",
+        prog="db_filler",
+        description="Fill db with fake datas",
     )
     parser.add_argument(
         "-n",
@@ -48,7 +51,8 @@ def main():
     delete_node_from_db(args.node_name)
 
     for res in db.nodes.find():
-       print(res)
+        print(res)
+
 
 if __name__ == "__main__":
     main()
