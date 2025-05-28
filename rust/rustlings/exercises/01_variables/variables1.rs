a9362e8793bf2c9dafb8f04a6620cb107030d31e
jpmondet <jp@mondet.org>
2025-05-28 10:55:06 +0200

    [rust] Started rustlings. Variables/funcs/ifs done

---
+++ rust/rustlings/exercises/01_variables/variables1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO: Add the missing keyword.
+    let x: i32 = 5;
+
+    println!("x has the value {x}");
+}
---
+++ rust/rustlings/exercises/01_variables/variables3.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32;
+
+    x = 5;
+
+    println!("Number {x}");
+}
